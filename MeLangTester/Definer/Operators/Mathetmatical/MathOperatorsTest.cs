35aca20662c882ad995743fb20a0deaf5aacaaac
Bluegent <bluegentleman@live.com>
2019-09-06 17:21:48 +0300

    Added test for divide operators

---
+++ MeLangTester/Definer/Operators/Mathetmatical/MathOperatorsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Language.Definer.Operators.Mathetmatical
+{
+    using MeLanguage.Definer;
+    using MeLanguage.Definer.Operators.Mathematical;
+    using MeLanguage.Types.Var;
+    using MeLanguage.Utility;
+
+    [TestClass]
+    public class MathOperatorsTest
+    {
+        [TestMethod]
+        public void DivideOperatorTest()
+        {
+            Operator divide = new DivideOperator().Divide;
+
+            float expected = 10.0f;
+            float result = divide.Execute(new[] { new MeNumber(100), new MeNumber(10), }).Get<float>();
+
+            Assert.AreEqual(expected,result);
+
+        }
+    }
+}
--- MeLanguage/Definer/Operators/Mathematical/DivideOperator.cs
+++ MeLanguage/Definer/Operators/Mathematical/DivideOperator.cs
@@ -4,18 +4,24 @@ namespace MeLanguage.Definer.Operators.Mathematical
     using System.Runtime.Remoting.Messaging;
 
     public class DivideOperator : IOperatorDefiner
-    { 
+    {
+        public Operator Divide { get; }
 
-        public void AddOperator(LanguageDefiner definer)
+        public DivideOperator()
         {
-            Operator divide = Utils.MakeOperator(LConstants.DIVIDE_OP, 2, true,
+            Divide = Utils.MakeOperator(LConstants.DIVIDE_OP, 2, true,
                 (values, op) =>
                     {
                         op.CheckParamCount(values.Length);
                         MeNumber result = values[0].Get<float>() / values[1].Get<float>();
                         return result;
                     }, CommonValidators.TwoNumbers, CommonParamTypes.TwoNumbers);
-            definer.AddOperator(divide);
+        }
+
+
+        public void AddOperator(LanguageDefiner definer)
+        {
+            definer.AddOperator(Divide);
         }
     }
 }
